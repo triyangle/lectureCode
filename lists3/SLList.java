125ed34f043c0fdf24eb6c3ef1da54b6efa1a30f
William Yang <willy.h.yang@gmail.com>
2017-02-01 20:47:53 -0800

    Remove trailing whitespace

--- lists3/SLList.java
+++ lists3/SLList.java
@@ -1,6 +1,6 @@
  /** An SLList is a list of integers, which hides the terrible truth
    * of the nakedness within. */
-public class SLList<LochNess> {	
+public class SLList<LochNess> {
 	private class StuffNode {
 		public LochNess item;
 		public StuffNode next;
@@ -9,7 +9,7 @@ public class SLList<LochNess> {
 			item = i;
 			next = n;
 		}
-	} 
+	}
 
 	private StuffNode first;
 	private int size;
@@ -27,7 +27,7 @@ public class SLList<LochNess> {
 
  	/** Returns the first item in the list. */
  	public LochNess getFirst() {
- 		return first.item; 		
+ 		return first.item;
  	}
 
  	/** Adds an item to the end of the list. */
