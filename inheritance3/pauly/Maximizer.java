5bca1190132eb66715133b85a7dc8029fb340277
William Yang <willy.h.yang@gmail.com>
2017-02-08 15:40:36 -0800

    Remove trailing whitespace

--- inheritance3/pauly/Maximizer.java
+++ inheritance3/pauly/Maximizer.java
@@ -3,10 +3,10 @@ public class Maximizer {
 		int maxDex = 0;
 		for (int i = 0; i < items.length; i += 1) {
 			if (items[i] > items[maxDex]) {
-				maxDex = i;                 
+				maxDex = i;
 			}
 		}
 		return items[maxDex];
 
 	}
-} 
+}
