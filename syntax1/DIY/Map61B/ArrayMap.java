cf8a23dc598ff1bbad9fda39d349fc0f8f6d1e21
William Yang <willy.h.yang@gmail.com>
2017-02-20 01:57:41 -0800

    Finish DIY

--- syntax1/DIY/Map61B/ArrayMap.java
+++ syntax1/DIY/Map61B/ArrayMap.java
@@ -1,4 +1,4 @@
-package Map61B;
+//package Map61B;
 
 import org.junit.Assert.*;
 import java.util.List;
@@ -12,48 +12,78 @@ import static org.junit.Assert.*;
  * An array based implementation of the Map61B class.
  */
 public class ArrayMap<K, V> implements Map61B<K, V> {
+
+    private K[] keys;
+    private V[] vals;
+    int size;
+
     public ArrayMap() {
+        keys = (K[]) new Object[100];
+        vals = (V[]) new Object[100];
     }
 
     /** Returns the index of the given key if it exists,
      *  -1 otherwise. */
     private int keyIndex(K key) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(keys[i])) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 
     public boolean containsKey(K key) {
-        return false;
+        return keyIndex(key) > -1;
     }
 
     public void put(K key, V value) {
-
+        int index = keyIndex(key);
+        if (index > -1) {
+            vals[index] = value;
+        } else {
+            keys[size] = key;
+            vals[size] = value;
+            size++;
+        }
     }
 
     public V get(K key) {
-        return false;
+        int index = keyIndex(key);
+        if (index == -1) {
+            throw new IndexOutOfBoundsException();
+        }
+        return vals[keyIndex(key)];
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public List<K> keys() {
-        return null;
+        List<K> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(keys[i]);
+        }
+        return list;
     }
 
-    /*@Test
+    @Test
     public void test() {
         ArrayMap<Integer, Integer> am = new ArrayMap<Integer, Integer>();
         am.put(2, 5);
         int expected = 5;
-        assertEquals(expected, am.get(2));
-    }*/
+        assertEquals((Integer) expected, am.get(2));
+        assertEquals(expected, (long) am.get(2));
+    }
 
     public static void main(String[] args) {
         ArrayMap<String, Integer> m = new ArrayMap<String, Integer>();
         m.put("horse", 3);
         m.put("fish", 9);
         m.put("house", 10);
+
+        jh61b.junit.TestRunner.runTests("all", ArrayMap.class);
     }
 }
--- syntax1/DIY/Map61B/MapHelper.java
+++ syntax1/DIY/Map61B/MapHelper.java
@@ -1,4 +1,4 @@
-package Map61B;
+//package Map61B;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -10,7 +10,23 @@ import java.util.List;
 public class MapHelper {
     /* Write the following three methods:
     /* get(Key) : Return item in map if it exists. */
+    public static <K, V> V get(Map61B<K, V> map, K key) {
+        return map.containsKey(key) ? map.get(key) : null;
+    }
+
     /* maxKey() : Returns max of all keys. Works only if x and y have comparable data. */
+    public static <K extends Comparable<K>, V> K maxKey(Map61B<K, V> map) {
+        List<K> keys = map.keys();
+        K max = keys.get(0);
+        for (int i = 1; i < keys.size(); i++) {
+            K current = keys.get(i);
+            if (current.compareTo(max) > 0) {
+                max = current;
+            }
+        }
+        return max;
+    }
+
     /* allBark(): Makes all keys bark, but only works for Dogs. */
 
     @Test
@@ -20,9 +36,11 @@ public class MapHelper {
         m.put("fish", 9);
         m.put("house", 10);
 
-        /*Integer actual = MapHelper.get(m, "fish");
+        Integer actual = MapHelper.get(m, "fish");
         Integer expected = 9;
-        assertEquals(expected, actual);*/
+        assertEquals(expected, actual);
+
+        assertEquals(null, MapHelper.get(m, "asdfasdf"));
     }
 
     @Test
@@ -32,8 +50,12 @@ public class MapHelper {
         m.put("fish", 9);
         m.put("house", 10);
 
-        /*String actual = MapHelper.maxKey(m);
+        String actual = MapHelper.maxKey(m);
         String expected = "house";
-        assertEquals(expected, actual);*/
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String... args) {
+        jh61b.junit.TestRunner.runTests("all", MapHelper.class);
     }
 }
